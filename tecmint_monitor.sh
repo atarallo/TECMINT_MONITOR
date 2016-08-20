c810fb41e0dc90211850563536d7a4e526580a9d
ALJI Mohamed <mr.m.alji@gmail.com>
2016-08-20 09:29:20 +0100

    why clear the screen

--- tecmint_monitor.sh
+++ tecmint_monitor.sh
@@ -9,9 +9,6 @@
 #! /bin/bash
 # unset any variable which system may be using
 
-# clear the screen
-clear
-
 unset tecreset os architecture kernelrelease internalip externalip nameserver loadaverage
 
 while getopts iv name
