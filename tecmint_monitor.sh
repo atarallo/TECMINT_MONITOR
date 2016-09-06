799f4e59727ef60bb5cbfd399128c203b61ce427
ALJI Mohamed <mr.m.alji@gmail.com>
2016-09-06 22:58:51 +0100

    ... synchronizing ...

--- tecmint_monitor.sh
+++ tecmint_monitor.sh
@@ -23,17 +23,17 @@ done
 if [[ ! -z $iopt ]]
 then
 {
-wd=$(pwd)
-basename "$(test -L "$0" && readlink "$0" || echo "$0")" > /tmp/scriptname
-scriptname=$(echo -e -n $wd/ && cat /tmp/scriptname)
-su -c "cp $scriptname /usr/bin/monitor" root && echo "Congratulations! Script Installed, now run monitor Command" || echo "Installation failed"
+	wd=$(pwd)
+	basename "$(test -L "$0" && readlink "$0" || echo "$0")" > /tmp/scriptname
+	scriptname=$(echo -e -n $wd/ && cat /tmp/scriptname)
+	su -c "cp $scriptname /usr/bin/monitor" root && echo "Congratulations! Script Installed, now run monitor Command" || echo "Installation failed"
 }
 fi
 
 if [[ ! -z $vopt ]]
 then
 {
-echo -e "tecmint_monitor version 0.1\nDesigned by Tecmint.com\nReleased Under Apache 2.0 License"
+	echo -e "tecmint_monitor version 0.1\nDesigned by Tecmint.com\nReleased Under Apache 2.0 License"
 }
 fi
 
