158ce8bbf860544d57ea980eb989ec5bc17f899d
Yuriy VG <yuravg@gmail.com>
2020-03-04 23:02:12 +0300

    Change of use of the script variables
    All the variables are made local,
    simplify naming of temp files.

--- tecmint_monitor.sh
+++ tecmint_monitor.sh
@@ -9,14 +9,8 @@
 #                                                                                                  #
 ####################################################################################################
 
-VERSION='0.2'
-
 # clear the screen
 clear
-
-# unset any variable which system may be using
-unset os_type architecture kernelrelease internalip externalip nameserver loadaverage
-
 
 SCRIPT_NAME="${BASH_SOURCE##*/}"
 help() {
@@ -27,7 +21,8 @@ help() {
 }
 
 show_version() {
-    printf "tecmint_monitor %s\n" "$VERSION"
+    local version='0.2'
+    printf "tecmint_monitor %s\n" "$version"
     printf "Designed by Tecmint.com\n"
     printf "Released Under Apache 2.0 License\n"
 }
@@ -45,71 +40,69 @@ while getopts "vuh?" name; do
     esac
 done
 
-GREEN="\E[32m"
-COLORRESET="\E[0m"
-
 msg() {
-    printf "%b$1 : %b $2\n" "$GREEN" "$COLORRESET"
+    local green="\E[32m"
+    local colorreset="\E[0m"
+
+    printf "%b$1 : %b $2\n" "$green" "$colorreset"
 }
 
 monitor() {
 
-    # Check OS Type
-    os_type=$(uname -o)
-    msg "OS Type" "${os_type}"
+    msg "OS" "$(uname -o)"
 
-    # Check OS Release Version and Name
-    OS=$(uname -s)
-    REV=$(uname -r)
-    MACH=$(uname -m)
+    local nkernel=$(uname -s)
+    local krelease=$(uname -r)
+    local mach=$(uname -m)
+    local distr=''
+    local pseudoname=''
+    local os_info=''
 
-    if [ "${OS}" = "SunOS" ]; then
-        OS=Solaris
-        ARCH=$(uname -p)
-        OSSTR="${OS} ${REV}(${ARCH} $(uname -v)"
-    elif [ "${OS}" = "AIX" ]; then
-        OSSTR="${OS} $(oslevel) ($(oslevel -r))"
-    elif [ "${OS}" = "Linux" ]; then
-        KERNEL=$(uname -r)
+    if [ "$nkernel" = "SunOS" ]; then
+        nkernel=Solaris
+        local arch=$(uname -p)
+        os_info="$nkernel $krelease($arch $(uname -v)"
+    elif [ "$nkernel" = "AIX" ]; then
+        os_info="$nkernel $(oslevel) ($(oslevel -r))"
+    elif [ "$nkernel" = "Linux" ]; then
+        nkernel=$(uname -r)
         if [ -f /etc/fedora-release ]; then
-            DIST='Fedora'
-            PSEUDONAME=$(cat /etc/fedora-release | sed s/.*\(// | sed s/\)//)
-            REV=$(cat /etc/fedora-release | sed s/.*release\ // | sed s/\ .*//)
+            distr='Fedora'
+            pseudoname=$(cat /etc/fedora-release | sed s/.*\(// | sed s/\)//)
+            krelease=$(cat /etc/fedora-release | sed s/.*release\ // | sed s/\ .*//)
         elif [ -f /etc/redhat-release ]; then
-            DIST='RedHat'
-            PSEUDONAME=$(cat /etc/redhat-release | sed s/.*\(// | sed s/\)//)
-            REV=$(cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//)
+            distr='RedHat'
+            pseudoname=$(cat /etc/redhat-release | sed s/.*\(// | sed s/\)//)
+            krelease=$(cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//)
         elif [ -f /etc/SuSE-release ]; then
-            DIST=$(cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//)
-            REV=$(cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //)
+            distr=$(cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//)
+            krelease=$(cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //)
         elif [ -f /etc/mandrake-release ]; then
-            DIST='Mandrake'
-            PSEUDONAME=$(cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//)
-            REV=$(cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//)
+            distr='Mandrake'
+            pseudoname=$(cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//)
+            krelease=$(cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//)
         elif [ -f /etc/debian_version ]; then
-            DIST='Debian'
-            # PSEUDONAME=$(cat /etc/-release | sed s/.*\(// | sed s/\)//)
-            # REV=$(cat /etc/fedora-release | sed s/.*release\ // | sed s/\ .*//)
-            DIST="Debian $(cat /etc/debian_version)"
-            REV=""
+            distr='Debian'
+            distr="Debian $(cat /etc/debian_version)"
+            krelease=""
         elif [ -f /etc/os-release ]; then
-            DIST=$(awk -F "PRETTY_NAME=" '{print $2}' /etc/os-release | tr -d '\n"')
+            distr=$(awk -F "PRETTY_NAME=" '{print $2}' /etc/os-release | tr -d '\n"')
         fi
-        if ${OSSTR} [ -f /etc/UnitedLinux-release ]; then
-            DIST="${DIST}[$(cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//)]"
+        if [ -f /etc/UnitedLinux-release ]; then
+            distr="${distr}[$(cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//)]"
         fi
 
-        OSSTR="${OS} ${DIST} ${REV}(${PSEUDONAME} ${KERNEL} ${MACH})"
+        os_info="$nkernel $distr $krelease($pseudoname $nkernel $mach)"
     fi
 
-    msg "OS Description" "$OSSTR"
+    msg "OS Description" "$os_info"
 
     # Check Architecture
-    architecture=$(uname -m)
+    local architecture=$(uname -m)
     msg "Architecture" "$architecture"
 
     # Check Kernel Release
-    kernelrelease=$(uname -r)
+    local kernelrelease=$(uname -r)
     msg "Kernel Release" "$kernelrelease"
 
     # Check hostname
@@ -123,56 +116,52 @@ monitor() {
     fi
 
     # Check Internal IP
-    internalip=$(hostname -I)
+    local internalip=$(hostname -I)
     msg "Internal IP" "$internalip"
 
     # Check External IP
     if hash dig 2>/dev/null; then
-        externalip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+        local EXTERNALIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
         msg "External IP" "$externalip"
     else
         msg "External IP" "NOTE: command 'dig' was not found in PATH"
     fi
 
     # Check DNS
-    nameservers=$(grep -v '#' /etc/resolv.conf | awk '{print $2}' | tr "\n" ' ')
+    local nameservers=$(grep -v '#' /etc/resolv.conf | awk '{print $2}' | tr "\n" ' ')
     msg "Name Servers" "$nameservers"
 
+    # Check Logged In Users
     if [ -n "$show_user" ]; then
-        # Check Logged In Users
         who>/tmp/who
         msg "Logged In users" "\n$(cat /tmp/who)"
         rm /tmp/who
     fi
 
     # Check RAM and SWAP Usages
-    free -h | grep -v + > /tmp/ramcache
+    local tecm_ramcache=/tmp/ramcache
+    free -h | grep -v + > "$tecm_ramcache"
     msg "Ram Usages"
-    grep -v "Swap" /tmp/ramcache
+    grep -v "Swap" "$tecm_ramcache"
     msg "Swap Usages"
-	grep -v "Mem"  /tmp/ramcache
+	grep -v "Mem"  "$tecm_ramcache"
+    rm "$tecm_ramcache"
 
     # Check Disk Usages
-    hddisk=$(lsblk | grep disk | awk '{print $1}')
-    df -h| grep "Filesystem\|/dev/${hddisk}*" > /tmp/diskusage
+    local tecm_diskusage=/tmp/diskusage
+    local hddisk=$(lsblk | grep disk | awk '{print $1}')
+    df -h| grep "Filesystem\|/dev/${hddisk}*" > "$tecm_diskusage"
     msg "Disk Usages"
-    cat /tmp/diskusage
+    cat "$tecm_diskusage"
+    rm "$tecm_diskusage"
 
     # Check Load Average
-    loadaverage=$(top -n 1 -b | grep "load average:" | awk '{print $(NF-2)" "$(NF-1)" "$NF}' | sed 's/, / /g')
+    local loadaverage=$(top -n 1 -b | grep "load average:" | awk '{print $(NF-2)" "$(NF-1)" "$NF}' | sed 's/, / /g')
     msg "Load Average" "$loadaverage"
 
     # Check System Uptime
-    tecuptime=$(uptime | awk '{print $3,$4}' | cut -f1 -d,)
+    local tecuptime=$(uptime | awk '{print $3,$4}' | cut -f1 -d,)
     msg "System Uptime Days/(HH:MM)" "$tecuptime"
-
-    # Unset Variables
-    unset os architecture kernelrelease internalip externalip nameserver loadaverage
-
-    # Remove Temporary Files
-    rm /tmp/ramcache /tmp/diskusage
 }
 
 monitor
-
-shift $((OPTIND -1))
